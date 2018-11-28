fb68069cfe205390990692eaad11df0c7820e4d3
Marie Laval <marie.laval@hec.edu>
2018-11-28 18:26:03 +0100

    update ingredients function

--- R/ingredients.R
+++ R/ingredients.R
@@ -1,19 +1,19 @@
 recipe_path <- "data/recipe_data.csv"
-#' 
 #'
 #' @return vector
 #'
 #' @import dplyr
 #' @importFrom tidytext unnest_tokens
+#' @importFrom readr read_csv
 #' 
 #' @export
-
 ingredients_list <- function(path){
-  recipe_data <- read.csv(path)
+  recipe_data <- read_csv(path)
   recipe_data_wide <- recipe_data %>% 
-  unnest_tokens(ingredient, recipeIngredients,token = 'regex', pattern=",") %>% 
-  mutate(ingredient = gsub("-"," ",ingredient))
-
-all_ingredients <- unique(recipe_data_wide$ingredient)
-return(all_ingredients)
+    unnest_tokens(ingredient, recipeIngredients,token = 'regex', pattern=",") %>% 
+    mutate(ingredient = gsub("-"," ",ingredient))
+  all_ingredients <- unique(recipe_data_wide$ingredient)
+  all_ingredients
 }
+
+ingredients_list(recipe_path)
