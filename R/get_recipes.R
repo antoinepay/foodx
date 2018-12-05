8fa2f4ef907f09a7bd7bc1a448e86941e3255cf4
antoinepayan <antoine.payan@hotmail.fr>
2018-12-05 20:16:19 +0100

    UPDATE - Improvements on for loops + adding marmiton recipes and ingredients as internal data

--- R/get_recipes.R
+++ R/get_recipes.R
@@ -1,144 +1,89 @@
-#' @import dplyr
-#' @import purrr
-
-#Sys.setlocale(locale="en_us.UTF-8")
-
-#' Temporary function, enables us to include or not
-#' all usual ingredients in the set of ingredients we will give to the machine
-#'
-#' @return
-#' @export
-#'
-#' @examples
-get_ingredient_you_always_have<-function(){
-  ingredients_you_always_have<-c("sucre","sel","beurre","poivre","huile-d-olive", "huile")
-}
-
-ingredients_you_always_have<-get_ingredient_you_always_have()
-
 remove_ingredients_you_always_have <- function(list){
-  list<-list[! (list %in% ingredients_you_always_have)]
+  list<-list[! (list %in% must_have_ingredients)]
   list
 }
 
-#' Give a list of 1538 lists (number of recipes)
-#' Within each list, there is the ingredients of the receipe
-#' 
-#' @return
+#' Quick way to obtain all the subsets given a set
+#'
+#' @param set
+#'
+#' @return list of subsets
 #' @export
 #'
-#' @examples
-get_list_of_unique<-function(){
-  list_of_unique <- strsplit(as.character(marmiton_recipes$recipeIngredients),',',fixed=TRUE)
-  list_of_unique <-lapply(list_of_unique, remove_ingredients_you_always_have)
-  list_of_unique
-}
-
-list_of_unique<-get_list_of_unique()
-
-
-#' Obtained from stackoverflow, a quick way to obtain all the subsets given a set
-#'
-#' @param set 
-#'
-#' @return list of subset
-#' @export
-#'
-#' @examples
-all.subsets.fast <- function(set) {
+get_subsets_from <- function(set) {
   n <- length(set)
-  bin <- vector(mode = "list", length = n)
-  for (i in 1L:n) {
-    bin[[i]] <- rep.int(c(rep.int(F, 2L ^ (i - 1L)),
-                          rep.int(T, 2L ^ (i - 1L))),
-                        2L ^ (n - i))
-  }
-  apply(do.call(cbind, bin), 1L, function(x) { set[x] } )
+  
+  bin <- map(1:n, function(i) {
+    rep.int(
+      c(rep.int(F, 2L ^ (i - 1L)),
+        rep.int(T, 2L ^ (i - 1L))),
+      2L ^ (n - i)
+    )
+  })
+  
+  apply(do.call(cbind, bin), 1L, function(x) { set[x] } )[-1]
 }
-
 
 #' The function that will look if something is contained in something
 #' if number of ingredients if too far (in percentage)
-#' from the number of ingredients in the receipe, is.contained don't count the containance.
+#' from the number of ingredients in the receipe, is_contained don't count the containance.
 #' The percentage parameter is set to 0 by default for reasons you will understand after
+#' @importFrom purrr map reduce
+#' @import dplyr
 #'
-#' @param vec1 
-#' @param vec2 
+#' @param v1 vector to be included in v2
+#' @param v2 vector to contain v1
 #' @param percentage 
 #'
-#' @return
+#' @return boolean
 #' @export
 #'
-#' @examples
-is.contained<-function(vec1, vec2, percentage = 0){
+is_contained <- function(v1, v2, percentage = 0) {
   
-  x <- vector(length = length(vec1))
+  minimum_length <- length(v2) * percentage / 100 
   
-  minimum_length <- length(vec2) * percentage / 100 
-  
-  if (length(vec1) < minimum_length) {    
+  if (length(v1) < minimum_length) {    
     return(FALSE)                         
-  }                                       
-  
-  for (i in 1:length(vec1)) {
-    
-    x[i] <- vec1[i] %in% vec2
-    
   }
-  all(x==T)
+  
+  map(v1, function(val) {
+    val %in% v2
+  }) %>% reduce(all)
 }
 
-
-
-
-#' Count number of appearance of one combination of ingredients in the list of unique
-#'
+#' Count the number of appearances of one combination of ingredients in the list of marmiton recipes
+#' @import dplyr
+#' @importFrom purrr map reduce
 #' @param one_combination 
 #' @param percentage 
 #'
-#' @return
 #' @export
-#'
-#' @examples
-get_number_for_one_combination<-function(one_combination,percentage){
-  
-  count <- 0
-  
-  for (i in 1:length(list_of_unique)){
-    
-    if (is.contained(one_combination,list_of_unique[[i]], percentage)) {
-      count=count+1
-    }
-    
-  }
-  
-  count
-}     
-
-
-
-
-
+get_number_for_one_combination <- function(one_combination, percentage) {
+  map(
+    marmiton_recipes_ingredients, 
+    function(recipe_ingredients) {
+      ifelse(is_contained(one_combination, recipe_ingredients, percentage), 1, 0)
+    }) %>% 
+    reduce(sum)
+}
 
 #' Calcul matrix of containance between different sets: for a given set i, m[i,j]=1 if the set j contains the set i
 #'
 #' @param all.subset 
 #'
-#' @return
+#' @return matrix of containance
 #' @export
 #'
-#' @examples
-get.matrix.subset.containance<-function(all.subset){
-  
+get.matrix.subset.containance<-function(all.subset)
+{
   matrix.subset<-matrix(nrow = length(all.subset),ncol=length(all.subset))
   
   for (i in 1:length(all.subset)){
-    matrix.subset[i,]<-sapply(all.subset, is.contained,vec1=all.subset[[i]])*1
+    matrix.subset[i,]<-sapply(all.subset, is_contained, v1=all.subset[[i]])*1
   }
   
   matrix.subset
 }
-
 
 
 #' Give all the subsets of the set according to the conditions wanted (minimum ingredient to use, must include ...)
@@ -148,30 +93,26 @@ get.matrix.subset.containance<-function(all.subset){
 #' @param usual.ing 
 #' @param must_include 
 #'
-#' @return
+#' @return subsets arranged in increasing length
 #' @export
 #'
-#' @examples
-preprocess.set.in.subset<-function(
+preprocess_set <- function(
   set,
   minimum_to_use,
   must_include
 ){
-  all.subset<-all.subsets.fast(set)
-  if (must_include!="nothing_bro"){
-    all.subset<-all.subset[sapply(all.subset, is.contained,vec1=must_include)]}
-  all.subset<-all.subset[lapply(all.subset, length) > minimum_to_use]
-  all.subset<-all.subset[order(sapply(all.subset,length),decreasing=F)]
-  all.subset
+  all_subsets <- get_subsets_from(set)
+  
+  if (must_include!="nothing_bro") {
+    all_subsets<-all_subsets[sapply(all_subsets, is_contained,v1=must_include)]
+  }
+  all_subsets<-all_subsets[lapply(all_subsets, length) > minimum_to_use]
+  all_subsets<-all_subsets[order(sapply(all_subsets,length),decreasing=F)]
+  all_subsets
 }
 
-
-
-
-
-
 #' Enables to iterate only on the subset that don't contain a subset with no appearances in the list of unique
-#' That is why we set percentage to 0 in the is.contained function.
+#' That is why we set percentage to 0 in the is_contained function.
 #' Indeed if it wasn't, we would not iterate on set that contains a subset with no appearance in the list of unique.
 #' Yet having no appearances in that list can mean, that the percentage rule was not passed,
 #' and not because it doesn't appear.
@@ -179,10 +120,8 @@ preprocess.set.in.subset<-function(
 #' @param matrix 
 #' @param i 
 #'
-#' @return
 #' @export
 #'
-#' @examples
 get_position_of_future_empty<-function(matrix,i){
   
   position<-rep(0,length(matrix[i,]))
@@ -196,10 +135,6 @@ get_position_of_future_empty<-function(matrix,i){
   position[position>0]
 }
 
-
-
-
-
 #' Give the number of appearances of each subset in the list of unique
 #' 
 #'
@@ -207,22 +142,20 @@ get_position_of_future_empty<-function(matrix,i){
 #' @param minimum_to_use 
 #' @param must_include 
 #'
-#' @return
 #' @export
 #'
-#' @examples
 get_number_of_each_combinations<-function(set,minimum_to_use,must_include){
   
-  all.subset<-preprocess.set.in.subset(set,minimum_to_use,must_include)
+  all_subsets <- preprocess_set(set, minimum_to_use,must_include)
   
-  a<-rep(0,length(all.subset))
+  a<-rep(0,length(all_subsets))
   
-  matrix<-get.matrix.subset.containance(all.subset)
+  matrix<-get.matrix.subset.containance(all_subsets)
   
-  for (i in 1:length(all.subset)){
+  for (i in 1:length(all_subsets)){
     
     if(matrix[i,i]!=0){
-      a[i]<-get_number_for_one_combination(all.subset[[i]],0)
+      a[i]<-get_number_for_one_combination(all_subsets[[i]],0)
       
       if (a[i]==0){
         index<-get_position_of_future_empty(matrix,i)
@@ -232,7 +165,7 @@ get_number_of_each_combinations<-function(set,minimum_to_use,must_include){
     }
     
   }
-  list(all.subset,a)
+  list(all_subsets,a)
 }
 
 
@@ -241,45 +174,35 @@ get_number_of_each_combinations<-function(set,minimum_to_use,must_include){
 #'
 #' @param list 
 #'
-#' @return
 #' @export
 #'
-#' @examples
 get_best_combinations<-function(list){
   index_vector<-list[[2]]>0
   list[[1]][index_vector]
 }
 
-
-
 #' returns the number of appearance of each interresting subset in the list of unique, 
 #' with taking into account - this time - the percentage of ingredient that cover the recepies 
-#' (e.g. if recipie is carotte oeuf fromage, the subset "carotte" covers 33% of the recipie)
-#' if percentage demanded is 40% than the subset "carotte" won't appear.
+#' (e.g. if recipie is carotte oeuf fromage, the subset "carotte" covers 33\% of the recipie)
+#' if percentage demanded is 40\% than the subset "carotte" won't appear.
 #'
-#' @param all.subset 
+#' @param all_subsets 
 #' @param percentage 
 #'
-#' @return
 #' @export
-#'
-#' @examples
-get_nb_best_combinations<-function(all.subset,percentage){
+get_nb_best_combinations<-function(all_subsets, percentage){
   
-  a<-rep(0,length(all.subset))
+  nb_occurences <- map(all_subsets, function(subset) {
+    get_number_for_one_combination(subset, percentage)
+  })
   
-  for (i in 1:length(all.subset)){
-    a[i]<-get_number_for_one_combination(all.subset[[i]],percentage)
-  }
-  
-  list(ingredients = all.subset, nb_occurences = a)
+  list(ingredients = all_subsets, nb_occurences = nb_occurences)
 }
-
-
 
 #' Calculate the best combination of ingredients
 #' i.e the one that appears the most in the list of unique, with respect to the parameters
 #' and returns a list of recipies containing this combination.
+#' Test with get_best_recipes_du_chef(c("carotte", "jambon", "fromage"), 20, 1, T)
 #'
 #' @param ingredients_to_use Set of ingredients
 #' @param proportion_of_recipe 
@@ -289,15 +212,12 @@ get_nb_best_combinations<-function(all.subset,percentage){
 #' @return List of recipes
 #' @export
 #'
-#' @examples get_best_recipes_du_chef(c("carotte", "jambon", "fromage"), 20, 1, T)
 get_best_recipes_du_chef <- function(
   ingredients_to_use, 
   proportion_of_recipe, 
   minimum_ingredients_to_use, 
   must_include
 ){
-  
-  
   new_minimum_to_use<-minimum_ingredients_to_use
   
   best_combinations <- try(get_number_of_each_combinations(
@@ -324,9 +244,9 @@ get_best_recipes_du_chef <- function(
   
   best_combination <- best_combinations$ingredients[which.max(best_combinations$nb_occurences)][[1]]
   
-  indexes <- list_of_unique %>% 
+  indexes <- get_list_of_unique() %>% 
     map_lgl(function(ingredients) {
-      is.contained(best_combination, ingredients, proportion_of_recipe)
+      is_contained(best_combination, ingredients, proportion_of_recipe)
     })
   
   if (new_minimum_to_use!=minimum_ingredients_to_use){
@@ -349,7 +269,6 @@ get_best_recipes_du_chef <- function(
 #' @return list of recipes 
 #' @export
 #'
-#' @examples
 get_best_receipe_using_most_ingredients<-function(
   ingredients_to_use, 
   must_include
@@ -364,9 +283,9 @@ get_best_receipe_using_most_ingredients<-function(
   
   best_combination <- best_combinations$ingredients[which.max(sapply(best_combinations$ingredients,length))][[1]]
   
-  indexes <- list_of_unique %>% 
+  indexes <- get_list_of_unique() %>% 
     map_lgl(function(ingredients) {
-      is.contained(best_combination, ingredients,0)
+      is_contained(best_combination, ingredients,0)
     })
   
   marmiton_recipes[indexes,]
@@ -386,7 +305,6 @@ get_best_receipe_using_most_ingredients<-function(
 #' @return list of recipes
 #' @export
 #'
-#' @examples
 get_receipe_with_least_ingredients_to_add<-function(
   ingredients_to_use, 
   minimum_ingredients_to_use, 
@@ -424,9 +342,9 @@ get_receipe_with_least_ingredients_to_add<-function(
   }
   best_combination <- best_combination_bis$ingredients[which.max(best_combination_bis$nb_occurences)][[1]]
   
-  indexes <- list_of_unique %>% 
+  indexes <- get_list_of_unique() %>% 
     map_lgl(function(ingredients) {
-      is.contained(best_combination, ingredients,v)
+      is_contained(best_combination, ingredients,v)
     })
   
   if (new_minimum_to_use!=minimum_ingredients_to_use){
@@ -446,10 +364,8 @@ get_receipe_with_least_ingredients_to_add<-function(
 #' @param minimum_ingredients_to_use 
 #' @param must_include 
 #'
-#' @return
 #' @export
 #'
-#' @examples
 get_best_recipies<-function(
   ingredients_to_use, 
   proportion_of_recipe=30, 
