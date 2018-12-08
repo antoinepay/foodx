04633a18e1595f1e9a41c65a44888a92798e87c2
antoinepayan <antoine.payan@hotmail.fr>
2018-12-08 18:55:03 +0100

    UPDATE - matches on product_name

--- R/matching.R
+++ R/matching.R
@@ -1,35 +1,24 @@
-library(stringr)
-library(readr)
-library(dplyr)
-library(purrr)
-
-
 #' Create a dataframe that splits up the product name
 #'
 #' @param df 
 #' @param col 
+#' 
+#' @importFrom purrr map keep
 #'
-#' @return
+#' @return data.frame
 #' @export
-#'
-#' @examples
-prepare_df <- function(df,col){
-  food_new <- df %>% 
-    mutate(list_of_product_name_words = strsplit(df$col," ")) %>% 
-      select(list_of_product_name_words)
-  food_new <- food_new %>% mutate(list_of_product_name_words = sapply(food_new$list_of_product_name_words,tolower),
-                                  total = sapply(food_new$list_of_product_name_words, function(x) length(unlist(strsplit(as.character(x), "\\W+")))),
-                                  first_word = map(food_new$list_of_product_name_words,1),
-                                  second_word = map(food_new$list_of_product_name_words,2),
-                                  last_word = map(food_new$list_of_product_name_words,last))
-return(food_new)
+prepare_df <- function(df, col_name) {
+  col_name <- enquo(col_name)
+  df <- df %>% 
+    as_tibble() %>% 
+    mutate(product_name_words = strsplit(tolower(!!col_name), "[\\ \\' \\,]"))
+  
+  df$product_name_words <- map(df$product_name_words, function(words) {
+    keep(words, function(word) nchar(word) >= 3)
+  })
+  
+  df
 }
-
-# Example:
-
-prepare_df(test_name,product_name)
-
-
 
 #' Insert columns in df created above with the matches between each column of the df above and the ingredient list
 #'
@@ -39,18 +28,13 @@ prepare_df(test_name,product_name)
 #' @return
 #' @export
 #'
-#' @examples
-build_df <- function(df,ingredient_vector){
-  df$match1 <- str_extract(df$first_word,paste(ingredient_vector,collapse = "|")),
-  df$match2 <- str_extract(df$second_word,paste(ingredient_vector,collapse = "|")),
-  df$match_last <- str_extract(df$last_word,paste(ingredient_vector,collapse = "|")),
-  return(df)
+build_df <- function(df, ingredient_vector) {
+  
+  df %>% 
+    mutate(matches = map(product_name_words, function(words) {
+      words %in% ingredient_vector 
+    }))
 }
-
-
-# Example:
-
-build_df(food_new,Ingredients_List)
 
 
 
@@ -63,7 +47,6 @@ build_df(food_new,Ingredients_List)
 #' @return
 #' @export
 #'
-#' @examples
 replace <- function(){
   replaced_product_name <- ifelse(match1 != is.na(), match1, ifelse(match2 != is.na(), match2, match_last))
 }
