c03fd9f315001e37be82b71f99c1f68bf8f70849
DESKTOP-E8FMMP7\Elliot <elliot.stocker@hotmail.fr>
2018-12-10 13:38:30 +0100

    List of Spices

---
+++ R/Epices.R
@@ -0,0 +1,12 @@
+library(rvest)
+my_page <- function(x){
+  x <- "https://fr.wikipedia.org/wiki/Herbes_et_aromates_de_cuisine"
+  read_html(x) %>% 
+  html_nodes("div.colonnes") %>%
+  html_nodes("li") %>%
+  html_text() %>% 
+  as.vector()
+}
+
+my_page()
+
