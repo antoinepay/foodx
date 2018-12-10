5b8cfbf22b0ec9f447f991f036978239e7972e2d
DESKTOP-E8FMMP7\Elliot <elliot.stocker@hotmail.fr>
2018-12-10 18:18:16 +0100

    recipe sorting functions update

--- R/recipe_sorting.R
+++ R/recipe_sorting.R
@@ -1,41 +1,41 @@
 #' Top 2 quickest recipes
 #'
-#' @param x 
+#' @param tibble 
 #'
 #' @return
 #' @export
 #'
 #' @examples
-sort_time <- function(x){
-  x <- marmiton_recipes$recipePreparationTime+marmiton_recipes$recipeCookingTime
+sort_time <- function(tibble){
+  x <- numeric(tibble$recipePreparationTime+tibble$recipeCookingTime, ordered = TRUE)
   order(x) %>% 
-    head(2)
+    head()
 }
 
 #' Top 2 cheapest recipes
 #'
-#' @param z 
+#' @param tibble 
 #'
 #' @return
 #' @export
 #'
 #' @examples
-sort_budget <- function(z){
-  z <- as.factor(marmiton_recipes$recipeCost)
+sort_budget <- function(tibble){
+  z <- factor(tibble$recipeCost, levels = c("bonmarche", "moyen", "assezcher"), ordered = TRUE)
   order(z) %>% 
-    head(2)
+    head()
 }
 
 #' Top 2 easiest recipes
 #'
-#' @param y 
+#' @param tibble 
 #'
 #' @return
 #' @export
 #'
 #' @examples
-sort_difficulty<- function(y){
-  y <- as.factor(marmiton_recipes$recipeDifficulty)
+sort_difficulty<- function(tibble){
+  y <- factor(tibble$recipeDifficulty, levels = c("tresfacile", "facile", "moyenne", "difficile"), ordered = TRUE)
   order(y) %>% 
-    head(2)
+    head()
 }
