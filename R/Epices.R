be98ddfea28e4cb843b352aabd490471d9f27e81
DESKTOP-E8FMMP7\Elliot <elliot.stocker@hotmail.fr>
2018-12-10 13:39:23 +0100

    spices with roxygen

--- R/Epices.R
+++ R/Epices.R
@@ -1,4 +1,12 @@
 library(rvest)
+#' Title
+#'
+#' @param x 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 my_page <- function(x){
   x <- "https://fr.wikipedia.org/wiki/Herbes_et_aromates_de_cuisine"
   read_html(x) %>% 
